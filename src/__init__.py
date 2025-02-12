18292da12c9e1039306b7e49d0933a9eedf054d3
Puneeth Y S <puneeth@Puneeths-MacBook-Pro.local>
2025-02-12 21:40:55 +0530

    added pytest and docker config

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,37 @@
+import os
 from flask import Flask
 from flask_restx import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 api = Api(app)
 
 # set config
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class Ping(Resource):
     def get(self):
         return {
             'status': 'success',
-            'message': 'ping pong.......!'
+            'message': 'ping pong....!'
         }
-    
+
 api.add_resource(Ping, '/ping')
