12c2f45416f54c1f0eb787bf4b080d90dc372b4f
Puneeth Y S <puneeth@Puneeths-MacBook-Pro.local>
2025-02-15 12:24:13 +0530

    Testing CI/CD flow

--- src/api/ping.py
+++ src/api/ping.py
@@ -7,7 +7,7 @@ api = Api(ping_blueprint)
 
 class Ping(Resource):
     def get(self):
-        return {"status": "success", "message": "ping pong.....!"}
+        return {"status": "success", "message": "ping pong ping pong...!"}
 
 
 api.add_resource(Ping, "/ping")
