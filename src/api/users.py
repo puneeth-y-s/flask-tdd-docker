a1d7c9938e93183e373e0d02bcc13e4a5a66d69c
Puneeth Y S <puneeth@Puneeths-MacBook-Pro.local>
2025-02-15 18:37:50 +0530

    added unit test case using monkeypatch pytest fixture

--- src/api/users.py
+++ src/api/users.py
@@ -1,16 +1,24 @@
 from flask import Blueprint, request
 from flask_restx import Api, Resource, fields
 
-from src import db
-from src.api.models import User
+# new
+from src.api.crud import (  # isort:skip
+    get_all_users,
+    get_user_by_email,
+    add_user,
+    get_user_by_id,
+    update_user,
+    delete_user,
+)
 
 users_blueprint = Blueprint("users", __name__)
 api = Api(users_blueprint)
 
+
 user = api.model(
     "User",
     {
-        "id": fields.Integer(readonly=True),
+        "id": fields.Integer(readOnly=True),
         "username": fields.String(required=True),
         "email": fields.String(required=True),
         "created_date": fields.DateTime,
@@ -19,6 +27,10 @@ user = api.model(
 
 
 class UsersList(Resource):
+    @api.marshal_with(user, as_list=True)
+    def get(self):
+        return get_all_users(), 200  # updated
+
     @api.expect(user, validate=True)
     def post(self):
         post_data = request.get_json()
@@ -26,66 +38,56 @@ class UsersList(Resource):
         email = post_data.get("email")
         response_object = {}
 
-        user = User.query.filter_by(email=email).first()
-
+        user = get_user_by_email(email)  # updated
         if user:
             response_object["message"] = "Sorry. That email already exists."
             return response_object, 400
 
-        db.session.add(User(username=username, email=email))
-        db.session.commit()
+        add_user(username, email)  # new
 
         response_object["message"] = f"{email} was added!"
         return response_object, 201
-
-    @api.marshal_with(user, as_list=True)
-    def get(self):
-        return User.query.all(), 200
 
 
 class Users(Resource):
     @api.marshal_with(user)
     def get(self, user_id):
-        user = User.query.filter_by(id=user_id).first()
+        user = get_user_by_id(user_id)  # updated
         if not user:
             api.abort(404, f"User {user_id} does not exist")
         return user, 200
 
+    @api.expect(user, validate=True)
+    def put(self, user_id):
+        post_data = request.get_json()
+        username = post_data.get("username")
+        email = post_data.get("email")
+        response_object = {}
+
+        user = get_user_by_id(user_id)  # updated
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+
+        if get_user_by_email(email):  # updated
+            response_object["message"] = "Sorry. That email already exists."
+            return response_object, 400
+
+        update_user(user, username, email)  # new
+
+        response_object["message"] = f"{user.id} was updated!"
+        return response_object, 200
+
     def delete(self, user_id):
         response_object = {}
-        user = User.query.filter_by(id=user_id).first()
+        user = get_user_by_id(user_id)  # updated
 
         if not user:
             api.abort(404, f"User {user_id} does not exist")
 
-        db.session.delete(user)
-        db.session.commit()
+        delete_user(user)  # new
 
         response_object["message"] = f"{user.email} was removed!"
         return response_object, 200
-
-    @api.expect(user, validate=True)
-    def put(self, user_id):
-        response_obj = {}
-        put_data = request.get_json()
-        username = put_data.get("username")
-        email = put_data.get("email")
-
-        user = User.query.filter_by(id=user_id).first()
-
-        if not user:
-            api.abort(404, f"User {user_id} does not exist")
-
-        if User.query.filter_by(email=email).first():
-            response_obj["message"] = "Sorry. That email already exists."
-            return response_obj, 400
-
-        user.username = username
-        user.email = email
-        db.session.commit()
-
-        response_obj["message"] = f"{user.id} was updated!"
-        return response_obj, 200
 
 
 api.add_resource(UsersList, "/users")
